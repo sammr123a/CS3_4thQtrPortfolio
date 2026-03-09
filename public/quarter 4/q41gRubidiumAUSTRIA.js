7933507ffa915a8c3430b67d451296dab9efa8a6
sammr123a <b2029stkaustria@pshs.edu.ph>
2026-03-10 00:40:20 +0800

    finished js

--- public/quarter 4/q41gRubidiumAUSTRIA.js
+++ public/quarter 4/q41gRubidiumAUSTRIA.js
@@ -1,1 +1,40 @@
 
+
+let listOfMovies=JSON.parse(localStorage.getItem("listOfMovies")) || [];
+
+const form=document.querySelector("form");
+
+form.addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    let rating = document.querySelector("input[name='rating']:checked").value; 
+
+    listOfMovies.push({
+        title: movieTitle.value,
+        year: year.value,
+        genre: movieGenre.value,
+        rating: rating
+     })
+        localStorage.setItem("listOfMovies", JSON.stringify(listOfMovies));
+        displayMovies(listOfMovies);
+    });
+
+
+function displayMovies(listOfMovies){
+    showList="";
+
+    for(i=0; i<listOfMovies.length; i++){
+        let idx = 0;
+        let stars= "";
+        while(idx < listOfMovies[i].rating){
+            stars += "⭐";
+            idx++;
+        }
+        showList += `<div class="listMovies">
+        ${listOfMovies[i].title} (${listOfMovies[i].year}) - ${listOfMovies[i].genre}, Rating: ${stars}
+        </div>`;
+    }
+        document.getElementById("listMovies").innerHTML = showList;
+}
+displayMovies(listOfMovies);
